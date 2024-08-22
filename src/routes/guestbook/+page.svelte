76a0c852192ec461192303e3630b1581ab660cd5
engageintellect <jc9361@gmail.com>
2024-08-21 19:40:23 -0700

    init

--- src/routes/guestbook/+page.svelte
+++ src/routes/guestbook/+page.svelte
@@ -183,11 +183,11 @@
 								// Apply a fade-in and scale-in animation to the new post
 								gsap.fromTo(
 									newPost,
-									{ opacity: 0, scale: 0.8 },
+									{ opacity: 0, y: 0 },
 									{
 										opacity: 1,
-										scale: 1,
-										duration: 1,
+										y: 0,
+										duration: 2,
 										ease: 'power4.out'
 									}
 								);
