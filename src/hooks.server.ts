fc962fda7ebffaf817440b3f28bde5fd5e8c0ed2
engageintellect <jc9361@gmail.com>
2024-08-27 11:58:54 -0700

    fix: persisting auth cookies during stripe redirects

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,31 +1,35 @@
-import { createInstance } from '$lib/pocketbase'
-import type { Handle } from '@sveltejs/kit'
+import { createInstance } from '$lib/pocketbase';
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const pb = createInstance()
+	const pb = createInstance();
 
-  // load the store data from the request cookie string
-  pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '')
-  try {
-    // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
-    if (pb.authStore.isValid) {
-      await pb.collection('users').authRefresh()
-    }
-  } catch (_) {
-    // clear the auth store on failed refresh
-    pb.authStore.clear()
-  }
+	// load the store data from the request cookie string
+	pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+	try {
+		// get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+		if (pb.authStore.isValid) {
+			await pb.collection('users').authRefresh();
+		}
+	} catch (_) {
+		// clear the auth store on failed refresh
+		pb.authStore.clear();
+	}
 
-  event.locals.pb = pb
-  event.locals.user = pb.authStore.model
+	event.locals.pb = pb;
+	event.locals.user = pb.authStore.model;
 
-  const response = await resolve(event)
+	const response = await resolve(event);
 
-  // send back the default 'pb_auth' cookie to the client with the latest store state
-  response.headers.set(
-    'set-cookie',
-    pb.authStore.exportToCookie({ httpOnly: false })
-  )
+	// send back the default 'pb_auth' cookie to the client with adjusted attributes
+	response.headers.set(
+		'set-cookie',
+		pb.authStore.exportToCookie({
+			httpOnly: true, // Keep it secure
+			sameSite: 'Lax', // Adjust for cross-site requests
+			secure: true // Ensure the site is accessed via HTTPS
+		})
+	);
 
-  return response
-}
+	return response;
+};
