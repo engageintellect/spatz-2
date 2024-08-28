3f4b219f8c8a86d9b19057818733070b8d61b171
engageintellect <jc9361@gmail.com>
2024-08-28 16:23:06 -0700

    init

--- src/lib/components/ui/Nav.svelte
+++ src/lib/components/ui/Nav.svelte
@@ -2,29 +2,25 @@
 	import { pb } from '$lib/pocketbase';
 	import ThemeToggle from '$lib/components/ui/ThemeToggle.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import Icon from '@iconify/svelte';
-	import { siteInfo } from '$lib/data';
 	import { onMount } from 'svelte';
 	import { gsap } from 'gsap';
 	import { currentUser } from '$lib/stores/user';
 	import { goto } from '$app/navigation';
-	import Avatar from '$lib/components/ui/Avatar.svelte';
-	import DropdownMenuItem from '$lib/components/ui/DropdownMenu.svelte';
 	import DropdownMenu from '$lib/components/ui/DropdownMenu.svelte';
 
 	let navHidden = true;
 
-	onMount(() => {
-		gsap
-			.timeline({
-				onStart: () => {
-					navHidden = false;
-				}
-			})
-			.from('.nav-logo', { delay: 0.5, opacity: 0, duration: 1 })
-			.from('.buttons', { delay: 0.5, opacity: 0, duration: 2 }, '-=1.5')
-			.from('.nav', { delay: 0.25, opacity: 0, duration: 0.75 }, '-=2.25');
-	});
+	//	onMount(() => {
+	//		gsap
+	//			.timeline({
+	//				onStart: () => {
+	//					navHidden = false;
+	//				}
+	//			})
+	//			.from('.nav-logo', { delay: 0.5, opacity: 0, duration: 1 })
+	//			.from('.buttons', { delay: 0.5, opacity: 0, duration: 2 }, '-=1.5')
+	//			.from('.nav', { delay: 0.25, opacity: 0, duration: 0.75 }, '-=2.25');
+	//	});
 
 	function handleLogout() {
 		pb.authStore.clear();
@@ -33,9 +29,7 @@
 	}
 </script>
 
-<nav
-	class={`nav sticky top-0 -z-[-1] border-b bg-background p-2 ${navHidden ? 'invisible' : 'visible'}`}
->
+<nav class={`nav sticky top-0 -z-[-1] border-b bg-background p-2 ${navHidden ? 'opacity-0' : ''}`}>
 	<div class="mx-auto flex w-full max-w-5xl items-center justify-between">
 		<a href="/" class="nav-logo" aria-label="Home">
 			<div class="px-2 text-2xl font-bold">spatz</div>
