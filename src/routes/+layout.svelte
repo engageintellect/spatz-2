46b37f2a5875c71ae275463e3680f5540d462d9a
engageintellect <jc9361@gmail.com>
2024-08-28 16:44:35 -0700

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,40 +9,26 @@
 	import Command from '$lib/components/ui/Command.svelte';
 	import { currentUser } from '$lib/stores/user';
 	import Toast from '$lib/components/ui/Toast.svelte';
-	import { afterNavigate } from '$app/navigation';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		console.log('Page loaded, forcing scroll to top');
-		window.scrollTo(0, 0);
-	});
-
-	afterNavigate(() => {
-		// Scroll to top of the page on navigation
-		if (typeof window !== 'undefined') {
-			window.scrollTo(0, 0);
-		}
-	});
 
 	export let data: PageData;
 
 	$: currentUser.set(data.user);
 
 	// START VIEW TRANSITIONS API
-	//	import { onNavigate } from '$app/navigation';
+	import { onNavigate } from '$app/navigation';
 
-	//	onNavigate((navigation) => {
-	// @ts-ignore <-- This is a private API so we need to ignore the TS error
-	//		if (!document.startViewTransition) return;
+	onNavigate((navigation) => {
+		// @ts-ignore <-- This is a private API so we need to ignore the TS error
+		if (!document.startViewTransition) return;
 
-	//		return new Promise((resolve) => {
-	// @ts-ignore <-- This is a private API so we need to ignore the TS error
-	//			document.startViewTransition(async () => {
-	//				resolve();
-	//				await navigation.complete;
-	//			});
-	//		});
-	//	});
+		return new Promise((resolve) => {
+			// @ts-ignore <-- This is a private API so we need to ignore the TS error
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 	// END VIEW TRANSITIONS API
 </script>
 
@@ -68,6 +54,5 @@
 		height: 100%;
 		margin: 0;
 		padding: 0;
-		overflow-x: hidden;
 	}
 </style>
