2e48bd6be5ac83bae3693fcd2e0feb8c8c32974e
engageintellect <jc9361@gmail.com>
2024-08-28 16:24:46 -0700

    init

--- src/lib/components/ui/Nav.svelte
+++ src/lib/components/ui/Nav.svelte
@@ -1,32 +1,23 @@
 <script lang="ts">
-	import { pb } from '$lib/pocketbase';
 	import ThemeToggle from '$lib/components/ui/ThemeToggle.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { onMount } from 'svelte';
 	import { gsap } from 'gsap';
 	import { currentUser } from '$lib/stores/user';
-	import { goto } from '$app/navigation';
 	import DropdownMenu from '$lib/components/ui/DropdownMenu.svelte';
 
 	let navHidden = true;
-
-	//	onMount(() => {
-	//		gsap
-	//			.timeline({
-	//				onStart: () => {
-	//					navHidden = false;
-	//				}
-	//			})
-	//			.from('.nav-logo', { delay: 0.5, opacity: 0, duration: 1 })
-	//			.from('.buttons', { delay: 0.5, opacity: 0, duration: 2 }, '-=1.5')
-	//			.from('.nav', { delay: 0.25, opacity: 0, duration: 0.75 }, '-=2.25');
-	//	});
-
-	function handleLogout() {
-		pb.authStore.clear();
-		localStorage.removeItem('chatMessages');
-		goto('/');
-	}
+	onMount(() => {
+		gsap
+			.timeline({
+				onStart: () => {
+					navHidden = false;
+				}
+			})
+			.from('.nav-logo', { delay: 0.5, opacity: 0, duration: 1 })
+			.from('.buttons', { delay: 0.5, opacity: 0, duration: 2 }, '-=1.5')
+			.from('.nav', { delay: 0.25, opacity: 0, duration: 0.75 }, '-=2.25');
+	});
 </script>
 
 <nav class={`nav sticky top-0 -z-[-1] border-b bg-background p-2 ${navHidden ? 'opacity-0' : ''}`}>
