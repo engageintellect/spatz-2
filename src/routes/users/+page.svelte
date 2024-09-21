ebb1d9a094000f879b3047753fbdeb06731121d8
engageintellect <jc9361@gmail.com>
2024-09-21 00:04:00 -0700

    cleaning up

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -7,9 +7,9 @@
 	import ScrollToTopButton from '$lib/components/ui/ScrollToTopButton.svelte';
 	import Icon from '@iconify/svelte';
 	import { animateMainStagger } from '$lib/animations';
-
 	import { gsap } from 'gsap';
 	import ScrollIndicator from '$lib/components/ui/ScrollIndicator.svelte';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 	export let data: any;
 
@@ -46,10 +46,8 @@
 		window.addEventListener('scroll', handleScroll);
 
 		if (typeof window !== 'undefined') {
-			const { gsap } = await import('gsap');
-			const { ScrollTrigger } = await import('gsap/ScrollTrigger');
-			gsap.registerPlugin(ScrollTrigger);
 			hidden = false;
+			gsap.registerPlugin(ScrollTrigger);
 
 			// Animate "book" on mount
 			gsap.fromTo(
