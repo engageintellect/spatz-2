dcd66f14f532b34ee2757f1a3b4fef918b728049
engageintellect <jc9361@gmail.com>
2024-09-27 22:36:05 -0700

    init

--- src/routes/checkout/payment/+page.svelte
+++ src/routes/checkout/payment/+page.svelte
@@ -1,18 +1,21 @@
-<!-- in src/routes/checkout/payment/+page.svelte -->
 <script lang="ts">
 	import { PUBLIC_STRIPE_KEY } from '$env/static/public';
 	import { onMount } from 'svelte';
 	import { loadStripe } from '@stripe/stripe-js';
 	import { Elements, PaymentElement } from 'svelte-stripe';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	let returnUrl = 'http://localhost:5173';
 
 	// data from server
 	export let data;
 
-	let clientSecret = PUBLIC_STRIPE_KEY;
-	let returnUrl = '/checkout/success';
-
 	// destructure server data
-	$: data;
+	$: ({ clientSecret } = data);
+
+	const appearance = {
+		theme: 'flat'
+	};
 
 	// Stripe instance
 	let stripe: any;
@@ -28,12 +31,17 @@
 
 	// handle form submission
 	async function submit() {
+		if (!stripe || !elements) {
+			console.error('Stripe or Elements is not loaded');
+			return;
+		}
+
 		// ask Stripe to confirm the payment
 		const { error } = await stripe.confirmPayment({
 			// pass instance that was used to create the Payment Element
 			elements,
 
-			// specify where to send the user when payment succeeeds
+			// specify where to send the user when payment succeeds
 			confirmParams: {
 				return_url: returnUrl
 			}
@@ -41,23 +49,34 @@
 
 		if (error) {
 			// handle error
-			console.error(error);
+			console.error('Payment confirmation error:', error);
 		}
 	}
 </script>
 
-<h1>Payment</h1>
+<div class="mx-auto max-w-2xl p-2 md:p-5">
+	<h1>Payment</h1>
 
-{#if stripe}
-	<form on:submit|preventDefault={submit}>
-		<!-- container for Stripe components -->
-		<Elements {stripe} {clientSecret} bind:elements>
-			<!-- display payment related fields -->
-			<PaymentElement />
-		</Elements>
+	{#if stripe}
+		<form on:submit|preventDefault={submit}>
+			<!-- container for Stripe components -->
+			<Elements
+				{stripe}
+				{clientSecret}
+				theme="flat"
+				labels="floating"
+				variables={{ colorPrimary: '#7c4dff' }}
+				bind:elements
+			>
+				<!-- display payment related fields -->
+				<PaymentElement />
+			</Elements>
 
-		<button>Pay</button>
-	</form>
-{:else}
-	Loading Stripe...
-{/if}
+			<div class="w-full max-w-md">
+				<Button class="w-full">Pay</Button>
+			</div>
+		</form>
+	{:else}
+		Loading Stripe...
+	{/if}
+</div>
